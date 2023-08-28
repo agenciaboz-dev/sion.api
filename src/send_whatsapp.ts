22b740de1feab50a75dd53075e860f5cfec0325c
ispadilha <israel.padilha@gmail.com>
2023-08-28 14:00:04 -0300

    update: whatsapp templates

--- src/send_whatsapp.ts
+++ src/send_whatsapp.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express"
 import { PrismaClient } from "@prisma/client"
 import { whatsapp } from "./whatsapp"
+import templates from "./templates/whatsapp_templates"
 const router = express.Router()
 const prisma = new PrismaClient()
 
@@ -11,8 +12,8 @@ router.post("/token", async (request: Request, response: Response) => {
     const prefix = number.slice(2, 4)
     const number2 = `55${prefix + number.slice(5)}`
 
-    const message = await whatsapp.sendMessage(number, `Token: ${data.token}`)
-    const message2 = await whatsapp.sendMessage(number2, `Token: ${data.token}`)
+    const message = await whatsapp.sendMessage(number, templates.token(data.token))
+    const message2 = await whatsapp.sendMessage(number2, templates.token(data.token))
     console.log(message)
     console.log(message2)
     response.json({ number1: message.body, number2: message2.body })
