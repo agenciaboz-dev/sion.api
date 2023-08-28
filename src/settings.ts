bef0ec85db33fcad2caee695ece2fc811420c710
nandobfer <nandobfer@yahoo.com.br>
2023-08-28 18:15:48 -0300

    test: log

--- src/settings.ts
+++ src/settings.ts
@@ -19,6 +19,7 @@ router.post("/rate", async (request: Request, response: Response) => {
 router.get("/whatsapp", async (request: Request, response: Response) => {
     const info = whatsapp.info
     console.log(info)
+    console.log(whatsapp)
     const qrcode = whatsappQrCode
     response.json({ info, qrcode })
 })
