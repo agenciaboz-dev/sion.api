250fbdfda47f143459756ddfed2a92901d4b344d
ispadilha <israel.padilha@gmail.com>
2023-08-28 14:00:28 -0300

    update: whatsapp templates ts file

---
+++ src/templates/whatsapp_templates.ts
@@ -0,0 +1,5 @@
+const token = (token:number|string) => `Utilize o seguinte token para confirmar sua assinatura: ${token}`
+
+const confirmacao = `Olá, sua assinatura foi confirmada. Obrigado!`
+
+export default {token, confirmacao}
