91f0b48d586af7a97a3c81bd7815efbe0e65e549
nandobfer <nandobfer@yahoo.com.br>
2023-08-28 19:06:38 -0300

    fix: limit type message

--- src/send_whatsapp.ts
+++ src/send_whatsapp.ts
@@ -12,12 +12,13 @@ router.post("/token", async (request: Request, response: Response) => {
     const prefix = number.slice(2, 4)
     const number2 = `55${prefix + number.slice(5)}`
 
-    const message = await whatsapp.sendMessage(number, templates.token(data.token, data.name, data.limit))
-    const message2 = await whatsapp.sendMessage(number2, templates.token(data.token, data.name, data.limit))
+    const formatedLimit = data.limit.toLocaleDateString("pt-br")
+
+    const message = await whatsapp.sendMessage(number, templates.token(data.token, data.name, formatedLimit))
+    const message2 = await whatsapp.sendMessage(number2, templates.token(data.token, data.name, formatedLimit))
     console.log(message)
     console.log(message2)
 
-    console.log(data.limit)
     response.json({ number1: message.body, number2: message2.body })
 })
 
