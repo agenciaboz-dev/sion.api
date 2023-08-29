5f02cb861233fe5a69d63b5ba14e02a12016d9ae
nandobfer <nandobfer@yahoo.com.br>
2023-08-29 12:01:34 -0300

    fix: typo

--- src/templates/whatsapp_templates.ts
+++ src/templates/whatsapp_templates.ts
@@ -8,9 +8,8 @@ const confirmacao = (contract: contracts, seller: users, signing: string) =>
         contract.name
     }\nTelefone: ${contract.phone}\nE-mail:${contract.email}\nVendedor: ${seller.name}\n\n*Assinando como:*\n${signing}`
 
-const assine = (signing: string, datelimit: string, link: string, contract: File) =>
-    `*Solicitação de Assinatura da Cooperativa Sion*\n\nClique no link abaixo para visualizar o contrato\n\n${link}\n\nEstará assinado como:\n${signing}\n\nData limite de assinatura:\n${datelimit}\n\n*Não compartilhe esta mensagem*\nPara sua segurança, não encaminhe este e-mail para ninguém.` +
-    `${contract}`
+const assine = (signing: string, datelimit: string, link: string) =>
+    `*Solicitação de Assinatura da Cooperativa Sion*\n\nClique no link abaixo para visualizar o contrato\n\n${link}\n\nEstará assinando como:\n${signing}\n\nData limite de assinatura:\n${datelimit}\n\n*Não compartilhe esta mensagem*\nPara sua segurança, não encaminhe este e-mail para ninguém.`
 
 const contato = (phone: number | string, email: string, name: string, message: string) =>
     `Olá ${name},\n\nAgradecemos por entrar em contato conosco por meio do formulário em nosso site. Estamos felizes em receber sua mensagem e gostaríamos de responder prontamente às suas perguntas e necessidades.\n\n*Detalhes da Mensagem*\n\nNome: ${name}\nE-mail: ${email}\nTelefone: ${phone}\nMensagem\n${message}\n\nEntraremos em contato com você o mais breve possível para fornecer as informações que você solicitou ou para discutir os detalhes mais aprofundados de sua mensagem. Sua opinião é importante para nós e estamos ansiosos para ajudá-lo da melhor maneira possível.\n\nAgradecemos novamente por se comunicar conosco e por considerar nossos serviços. Tenha um ótimo dia!`
