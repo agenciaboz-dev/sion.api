6e3653223fdacd9119ecca4c0e0cea32ee11aa72
nandobfer <nandobfer@yahoo.com.br>
2023-08-29 11:59:06 -0300

    feat: added signed confirmation zapzap

--- src/send_whatsapp.ts
+++ src/send_whatsapp.ts
@@ -5,13 +5,17 @@ import templates from "./templates/whatsapp_templates"
 const router = express.Router()
 const prisma = new PrismaClient()
 
-router.post("/token", async (request: Request, response: Response) => {
-    const data = request.body
-    const number = `55${data.number}@c.us`
+const getNumbers = (original_number: string | number) => {
+    const number = `55${original_number}@c.us`
 
     const prefix = number.slice(2, 4)
     const number2 = `55${prefix + number.slice(5)}`
+    return [number, number2]
+}
 
+router.post("/token", async (request: Request, response: Response) => {
+    const data = request.body
+    const [number, number2] = getNumbers(data.number)
 
     const message = await whatsapp.sendMessage(number, templates.token(data.token, data.name, data.limit))
     const message2 = await whatsapp.sendMessage(number2, templates.token(data.token, data.name, data.limit))
@@ -23,15 +27,28 @@ router.post("/token", async (request: Request, response: Response) => {
 
 router.post("/send", async (request: Request, response: Response) => {
     const data = request.body
-    const number = `55${data.number}@c.us`
-
-    const prefix = number.slice(2, 4)
-    const number2 = `55${prefix + number.slice(5)}`
+    const [number, number2] = getNumbers(data.number)
 
     const message = await whatsapp.sendMessage(number, data.message)
     const message2 = await whatsapp.sendMessage(number2, data.message)
 
     response.json({ message, message2 })
+})
+
+router.post("/signed", async (request: Request, response: Response) => {
+    const data = request.body
+    const [number, number2] = getNumbers(data.number)
+
+    const contract = await prisma.contracts.findUnique({ where: { id: data.id }, include: { seller: true } })
+
+    if (contract) {
+        const message = await whatsapp.sendMessage(number, templates.confirmacao(contract, contract.seller, data.signing))
+        const message2 = await whatsapp.sendMessage(number2, templates.confirmacao(contract, contract.seller, data.signing))
+
+        response.json({ message, message2 })
+    } else {
+        response.json({ error: "contract not found" })
+    }
 })
 
 export default router
