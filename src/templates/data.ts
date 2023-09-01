e5a8dc49b8f6e934d4ad0f0faa778f66a4f3c8fa
MizaelAgenciaBoz <mizael@agenciazop.com.br>
2023-09-01 15:53:07 -0300

    updated templates v1

---
+++ src/templates/data.ts
@@ -0,0 +1,7 @@
+export const data = {
+    instagram_link:``,
+    facebook_link:``,
+    linkedin_link:``,
+    address:``,
+    address_link:``,
+}
---
+++ src/templates/images.ts
@@ -0,0 +1,12 @@
+const url = `https://app.agenciaboz.com.br:4101/static/email`
+export const images = {
+    header_logo:`${url}/header_logo.png`,
+    footer_logo: `${url}/header_logo.png`,
+    footer_logo_dark: `${url}/header_logo_dark.png`,
+    facebook: `${url}/facebook.png`,
+    facebook_dark: `${url}/facebook.png`,
+    instagram: `${url}/instagram.png`,
+    instagram_dark: `${url}/instagram.png`,
+    linkedin: `${url}/linkedin.png`,
+    linkedin_dark: `${url}/linkedin.png`,
+}
