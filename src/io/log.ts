bfd08c271fc139f109ac1dd4be9c90f13455416d
nandobfer <nandobfer@yahoo.com.br>
2023-09-04 16:58:38 -0300

    update: try catching logs creation

--- src/io/log.ts
+++ src/io/log.ts
@@ -3,13 +3,17 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 const log = async (text: string, id: number) => {
-    await prisma.userLogs.create({
-        data: {
-            date: new Date(),
-            text: text,
-            user_id: id,
-        },
-    })
+    try {
+        await prisma.userLogs.create({
+            data: {
+                date: new Date(),
+                text: text,
+                user_id: id,
+            },
+        })
+    } catch {
+        console.log(`error creating log with data: ${{ text, id }}`)
+    }
 }
 
 export default log
