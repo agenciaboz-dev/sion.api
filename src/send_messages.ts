4c25373a811720255223c25e1886cef37010cc32
nandobfer <nandobfer@yahoo.com.br>
2023-09-04 16:42:12 -0300

    update: handling with possibly unconnected zapzap

--- src/send_messages.ts
+++ src/send_messages.ts
@@ -19,96 +19,106 @@ export const getNumbers = (original_number: string | number) => {
 }
 
 router.post("/token", async (request: Request, response: Response) => {
-    const data = request.body
-    const [number, number2] = getNumbers(data.number)
+    if (whatsapp.info) {
+        const data = request.body
+        const [number, number2] = getNumbers(data.number)
 
-    const message = await whatsapp.sendMessage(number, templates.token(data.token, data.name, data.limit))
-    const message2 = await whatsapp.sendMessage(number2, templates.token(data.token, data.name, data.limit))
-    console.log(message)
-    console.log(message2)
-
-    const signing =
-        (await prisma.contracts.findFirst({ where: { phone: data.number }, orderBy: { id: "desc" } })) ||
-        (await prisma.users.findFirst({ where: { phone: data.number } }))
-
-    if (signing) {
-        sendMail(
-            signing.email,
-            `TOKEN: ${data.token} - Token de verificação de assinatura`,
-            `TOKEN: ${data.token} - Token de verificação de assinatura`,
-            email_token(data.limit, signing.email)
-        )
-    }
-
-    response.json({ number1: message.body, number2: message2.body })
-})
-
-router.post("/send", async (request: Request, response: Response) => {
-    const data = request.body
-    const [number, number2] = getNumbers(data.number)
-
-    const message = await whatsapp.sendMessage(number, data.message, { linkPreview: true })
-    const message2 = await whatsapp.sendMessage(number2, data.message, { linkPreview: true })
-    // const signing = await prisma.contracts.findFirst({where: {phone: data.number}, orderBy:{id:"desc"}}) || await prisma.users.findFirst({where: {phone: data.number}})
-
-    response.json({ message, message2 })
-})
-
-router.post("/signed", async (request: Request, response: Response) => {
-    const data = request.body
-    const [number, number2] = getNumbers(data.number)
-
-    const contract = await prisma.contracts.findUnique({ where: { id: Number(data.id) }, include: { seller: true } })
-
-    console.log({ data, contract })
-
-    if (contract) {
-        const message = await whatsapp.sendMessage(number, templates.confirmacao(contract, contract.seller, data.signing))
-        const message2 = await whatsapp.sendMessage(number2, templates.confirmacao(contract, contract.seller, data.signing))
+        const message = await whatsapp.sendMessage(number, templates.token(data.token, data.name, data.limit))
+        const message2 = await whatsapp.sendMessage(number2, templates.token(data.token, data.name, data.limit))
+        console.log(message)
+        console.log(message2)
 
         const signing =
             (await prisma.contracts.findFirst({ where: { phone: data.number }, orderBy: { id: "desc" } })) ||
             (await prisma.users.findFirst({ where: { phone: data.number } }))
-        sendMail(
-            signing!.email,
-            "Assinatura confirmada",
-            "Olá! Sua assinatura foi confirmada.",
-            email_confirmacao_assinatura(contract, contract.seller, signing!.email)
-        )
+
+        if (signing) {
+            sendMail(
+                signing.email,
+                `TOKEN: ${data.token} - Token de verificação de assinatura`,
+                `TOKEN: ${data.token} - Token de verificação de assinatura`,
+                email_token(data.limit, signing.email)
+            )
+        }
+
+        response.json({ number1: message.body, number2: message2.body })
+    }
+})
+
+router.post("/send", async (request: Request, response: Response) => {
+    if (whatsapp.info) {
+        const data = request.body
+        const [number, number2] = getNumbers(data.number)
+
+        const message = await whatsapp.sendMessage(number, data.message, { linkPreview: true })
+        const message2 = await whatsapp.sendMessage(number2, data.message, { linkPreview: true })
+        // const signing = await prisma.contracts.findFirst({where: {phone: data.number}, orderBy:{id:"desc"}}) || await prisma.users.findFirst({where: {phone: data.number}})
 
         response.json({ message, message2 })
-    } else {
-        response.json({ error: "contract not found" })
+    }
+})
+
+router.post("/signed", async (request: Request, response: Response) => {
+    if (whatsapp.info) {
+        const data = request.body
+        const [number, number2] = getNumbers(data.number)
+
+        const contract = await prisma.contracts.findUnique({ where: { id: Number(data.id) }, include: { seller: true } })
+
+        console.log({ data, contract })
+
+        if (contract) {
+            const message = await whatsapp.sendMessage(number, templates.confirmacao(contract, contract.seller, data.signing))
+            const message2 = await whatsapp.sendMessage(number2, templates.confirmacao(contract, contract.seller, data.signing))
+
+            const signing =
+                (await prisma.contracts.findFirst({ where: { phone: data.number }, orderBy: { id: "desc" } })) ||
+                (await prisma.users.findFirst({ where: { phone: data.number } }))
+            sendMail(
+                signing!.email,
+                "Assinatura confirmada",
+                "Olá! Sua assinatura foi confirmada.",
+                email_confirmacao_assinatura(contract, contract.seller, signing!.email)
+            )
+
+            response.json({ message, message2 })
+        } else {
+            response.json({ error: "contract not found" })
+        }
     }
 })
 
 router.post("/contract", async (request: Request, response: Response) => {
-    const data = request.body
+    if (whatsapp.info) {
+        const data = request.body
 
-    const [number, number2] = getNumbers(data.number)
+        const [number, number2] = getNumbers(data.number)
 
-    const message = await whatsapp.sendMessage(number, templates.assine(data.signing, data.limit, data.link), { linkPreview: true })
-    const message2 = await whatsapp.sendMessage(number2, templates.assine(data.signing, data.limit, data.link), { linkPreview: true })
+        const message = await whatsapp.sendMessage(number, templates.assine(data.signing, data.limit, data.link), { linkPreview: true })
+        const message2 = await whatsapp.sendMessage(number2, templates.assine(data.signing, data.limit, data.link), { linkPreview: true })
 
-    sendMail(data.signing, "contrato", email_assinatura(data.signing, data.limit, data.link))
+        sendMail(data.signing, "contrato", email_assinatura(data.signing, data.limit, data.link))
 
-    response.json({ message, message2 })
+        response.json({ message, message2 })
+    }
 })
 
 router.post("/new", async (request: Request, response: Response) => {
-    const data = request.body
+    if (whatsapp.info) {
+        const data = request.body
 
-    const [number, number2] = getNumbers(data.number)
+        const [number, number2] = getNumbers(data.number)
 
-    const contract = await prisma.contracts.findUnique({ where: { id: Number(data.id) }, include: { seller: true } })
+        const contract = await prisma.contracts.findUnique({ where: { id: Number(data.id) }, include: { seller: true } })
 
-    if (contract) {
-        const message = await whatsapp.sendMessage(number, templates.cadastrado(contract, contract.seller))
-        const message2 = await whatsapp.sendMessage(number2, templates.cadastrado(contract, contract.seller))
+        if (contract) {
+            const message = await whatsapp.sendMessage(number, templates.cadastrado(contract, contract.seller))
+            const message2 = await whatsapp.sendMessage(number2, templates.cadastrado(contract, contract.seller))
 
-        response.json({ message, message2 })
-    } else {
-        response.json({ error: "contract not found" })
+            response.json({ message, message2 })
+        } else {
+            response.json({ error: "contract not found" })
+        }
     }
 })
 
