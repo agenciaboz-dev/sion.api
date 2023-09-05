d3aa4b43ce60c84f7180bd439ad7c816ef8e6715
MizaelAgenciaBoz <mizael@agenciazop.com.br>
2023-09-05 10:28:17 -0300

    fix images

--- src/templates/images.ts
+++ src/templates/images.ts
@@ -4,9 +4,9 @@ export const images = {
     footer_logo: `${url}/footer_logo.png`,
     footer_logo_dark: `${url}/footer_logo_dark.png`,
     facebook: `${url}/facebook.png`,
-    facebook_dark: `${url}/facebook.png`,
+    facebook_dark: `${url}/facebook_dark.png`,
     instagram: `${url}/instagram.png`,
-    instagram_dark: `${url}/instagram.png`,
+    instagram_dark: `${url}/instagram_dark.png`,
     linkedin: `${url}/linkedin.png`,
-    linkedin_dark: `${url}/linkedin.png`,
+    linkedin_dark: `${url}/linkedin_dark.png`,
 }
