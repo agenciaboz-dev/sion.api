33024dd19a2b7651bc0a1dbf4cb336cab6c5b2dc
MizaelAgenciaBoz <mizael@agenciazop.com.br>
2023-09-05 10:26:22 -0300

    fix dark logo, and logo

--- src/templates/images.ts
+++ src/templates/images.ts
@@ -1,8 +1,8 @@
 const url = `https://app.agenciaboz.com.br:4101/static/email`
 export const images = {
     header_logo:`${url}/header_logo.png`,
-    footer_logo: `${url}/header_logo.png`,
-    footer_logo_dark: `${url}/header_logo_dark.png`,
+    footer_logo: `${url}/footer_logo.png`,
+    footer_logo_dark: `${url}/footer_logo_dark.png`,
     facebook: `${url}/facebook.png`,
     facebook_dark: `${url}/facebook.png`,
     instagram: `${url}/instagram.png`,
