6de06050b7938fbc5b717fb70591957ee7df6dbb
nandobfer <nandobfer@yahoo.com.br>
2023-09-05 10:26:57 -0300

    update: hidden zapzap qrcode on terminal

--- src/whatsapp.ts
+++ src/whatsapp.ts
@@ -13,7 +13,7 @@ export const whatsapp = new Client({
 
 whatsapp.on("qr", (qr) => {
     whatsappQrCode = qr
-    qrcode.generate(qr, { small: true })
+    // qrcode.generate(qr, { small: true })
     const io = getIoInstance()
 
     io.emit("zap:qrcode", qr)
